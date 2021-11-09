d26be9946ffba23c88c57bb0ef2a0c2c5cd71951
Sinhalaya Official ✅ <89065560+SinhalayaCreator@users.noreply.github.com>
2021-11-09 17:20:44 +0530

    Delete alive.py

--- plugins/alive.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, Filters
-
-@Client.on_message(Filters.command(["alive"]))
-async def start(client, message):
-    alivemsg = f"Queen Zellie is online now"
-    await message.reply_text(alivemsg)
